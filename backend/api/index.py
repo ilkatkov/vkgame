4e82ebf0789040a7ebc10ec606f67cad9573701e
Ivan Roznov <91953084+Luaneo@users.noreply.github.com>
2024-02-04 13:06:33 +0000

    user/create route added

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, Request
+from prisma import Prisma
 
 app = Flask(__name__)
 
@@ -11,3 +12,22 @@ def home():
 @app.route("/about")
 def about():
     return "About"
+
+
+@app.route("/user/create", methods=["POST"])
+async def create_user(request: Request):
+    user_id = request.headers.get("user_id")
+
+    db = Prisma()
+    await db.connect()
+
+    try:
+        user = await db.user.create({"id": user_id})
+        result = {"status": "ok", "user_id": user.id}
+
+    except:
+        result = {"status": "error", "user": None}
+
+    finally:
+        await db.disconnect()
+        return result
