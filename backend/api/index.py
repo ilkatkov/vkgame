d7d719d1fd084d2f4af9b0c2d34b3f936df25d8f
Foplir <lolkekkro@gmail.com>
2024-02-18 13:11:49 +0400

    first test

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,5 +1,4 @@
-from flask import Flask, Request
-from prisma import Prisma
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -27,6 +26,58 @@ async def create_user(request: Request):
 
     except:
         result = {"status": "error", "user": None}
+
+    finally:
+        await db.disconnect()
+        return result
+
+
+@app.route("/game/create")
+async def create_game(request: Request):
+    game_id = request.headers.get("id")
+
+    db = Prisma()
+    await db.connect()
+
+    try:
+        user = await db.game.create({"id": game_id})
+        result = {"status": "ok", "user_id": user.id}
+
+    except:
+        result = {"status": "error", "user": None}
+
+    finally:
+        await db.disconnect()
+        return result
+
+
+@app.route("/game/get")
+async def get_game(request: Request):
+    game_id = request.headers.get("id")
+
+    db = Prisma()
+    await db.connect()
+    
+    try:
+        game = await db.game.findUnique({
+        where: {
+            id: game_id,
+        },
+        select: {
+            gameType: true,
+            cards: true,
+            matchCards : true,
+            theme : true,
+            subject : true,
+            ownerId : true
+        },
+        })
+
+        result = {"status": "ok", "gameType": game.gameType, "cards" : game.cards, "matchCards" : game.matchCards, "theme" : game.theme, "subject" : game.subject, "ownerId" : game.ownerId}
+
+
+    except:
+        result = {"status": "error"}
 
     finally:
         await db.disconnect()
