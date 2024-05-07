75c71ef28e19e2b1f506e9da864df881aaa47207
Blobik1 <amir.dab.2008@gmail.com>
2024-05-07 18:34:44 +0400

    Добавление комментариев к коду

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,6 +1,5 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 from .models import *
 from prisma import Prisma
 import prisma.errors
@@ -18,15 +17,14 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_headers=["*"], allow_methods=["*"]
-)
-
-# User-realted methods
 
 
+# User-related methods
+
+# Создание пользователя
 @app.post("/user/{user_id}")
 async def create_user(user_id: int) -> prisma.models.User:
+
     user = await db.user.upsert(
         where={"id": user_id},
         data={
@@ -36,20 +34,28 @@ async def create_user(user_id: int) -> prisma.models.User:
             "update": {},
         },
     )
+
     return user
 
 
+# Получение игр пользователя (для админов)
 @app.get("/user/{user_id}/games")
 async def get_owned_games(user_id: int) -> list[prisma.models.Game]:
+
     games = await db.game.find_many(where={"ownerId": user_id})
+
     return games
+
+
 
 
 # Game-related methods
 
-
+# Получение игры
 @app.get("/game/{game_id}")
 async def get_game(game_id: int) -> prisma.models.Game:
+    
+    # Поиск игры
     game = await db.game.find_unique(
         where={
             "id": game_id,
@@ -57,6 +63,7 @@ async def get_game(game_id: int) -> prisma.models.Game:
         include={"classicCards": True, "matchCards": True},
     )
 
+    # Проверка на наличие игры
     if game is None:
         raise HTTPException(
             status_code=404, detail="No record matching the given input could be found"
@@ -65,8 +72,11 @@ async def get_game(game_id: int) -> prisma.models.Game:
     return game
 
 
+# Создание игры
 @app.post("/game", status_code=201)
 async def create_game(request: CreateGameRequestModel) -> prisma.models.Game:
+    
+    # Создание записи
     game = await db.game.create(
         {
             "owner": {"connect": {"id": request.ownerId}},
@@ -101,23 +111,31 @@ async def create_game(request: CreateGameRequestModel) -> prisma.models.Game:
             },
         }
     )
+
     return game
 
 
+# Удаление игры
 @app.delete("/game/{game_id}")
 async def delete_game(game_id: int) -> DeleteGameResponseModel:
+
     cardsDeleted = await db.classiccard.delete_many({"gameId": game_id})
     cardsDeleted += await db.matchcard.delete_many({"gameId": game_id})
     deletedGame = await db.game.delete({"id": game_id})
+
+    # Проверка на наличие игры к удалению
     if deletedGame is None:
         raise HTTPException(status_code=404, detail="Could not find a record to delete")
+    
     return {"cardsDeleted": cardsDeleted, "deletedGame": deletedGame}
 
 
+# Изменение игры
 @app.put("/game/{game_id}")
 async def modify_game(
-    game_id: int, request: ModifyGameRequestModel
-) -> prisma.models.Game:
+    game_id: int, 
+    request: ModifyGameRequestModel) -> prisma.models.Game:
+    
     query = request.model_dump()
     keysToDelete = []
     for key in query:
@@ -126,18 +144,22 @@ async def modify_game(
     for key in keysToDelete:
         query.pop(key)
     game = await db.game.update(where={"id": game_id}, data=query)
+    
+    # Проверка наличия игры
     if game is None:
         raise HTTPException(status_code=404, detail="No record could be found")
+    
     return game
 
 
 # Card-related methods
 
-
+# Создание карточки
 @app.post("/classiccard")
 async def create_classic_card(
     request: CreateClassicCardRequestModel,
-) -> prisma.models.ClassicCard:
+    ) -> prisma.models.ClassicCard:
+    # Создание записи
     card = await db.classiccard.create(
         {
             "game": {"connect": {"id": request.gameId}},
@@ -145,21 +167,29 @@ async def create_classic_card(
             "description": request.description,
         }
     )
+
     return card
 
 
+# Удаление карточки
 @app.delete("/classiccard/{card_id}")
 async def delete_classic_card(card_id: int) -> prisma.models.ClassicCard:
+    
     card = await db.classiccard.delete({"id": card_id})
+    
+    # Проверка на наличие удаляемой карточки
     if card is None:
         raise HTTPException(status_code=404, detail="Could not find a record to delete")
+    
     return card
 
 
+# Изменение карточки
 @app.put("/classiccard/{card_id}")
 async def modify_classic_card(
     card_id: int, request: ModifyClassicCardRequestModel
-) -> prisma.models.ClassicCard:
+    ) -> prisma.models.ClassicCard:
+
     query = request.model_dump()
     keysToDelete = []
     for key in query:
@@ -168,15 +198,21 @@ async def modify_classic_card(
     for key in keysToDelete:
         query.pop(key)
     card = await db.classiccard.update(where={"id": card_id}, data=query)
+
+    # Проверка на наличие изменяемой игры
     if card is None:
         raise HTTPException(status_code=404, detail="No record could be found")
     return card
 
 
+# MatchCard-related methods
+
+# Создание match-карточки
 @app.post("/matchcard")
 async def create_match_card(
     request: CreateMatchCardRequestModel,
 ) -> prisma.models.MatchCard:
+    
     card = await db.matchcard.create(
         {
             "game": {
@@ -189,29 +225,39 @@ async def create_match_card(
             "description": request.description,
         }
     )
+
     return card
 
 
+# Удаление match-карточки
 @app.delete("/matchcard/{card_id}")
-async def delete_match_card(card_id: int) -> prisma.models.MatchCard:
+async def delete_classic_card(card_id: int) -> prisma.models.MatchCard:
+    
     card = await db.matchcard.delete({"id": card_id})
     if card is None:
         raise HTTPException(status_code=404, detail="Could not find a record to delete")
+    
     return card
 
 
+# Изменение match-карточки
 @app.put("/matchcard/{card_id}")
-async def modify_match_card(
+async def modify_classic_card(
     card_id: int, request: ModifyMatchCardRequestModel
-) -> prisma.models.MatchCard:
+    ) -> prisma.models.MatchCard:
+
     query = request.model_dump()
     keysToDelete = []
+
     for key in query:
         if not query[key]:
             keysToDelete.append(key)
     for key in keysToDelete:
         query.pop(key)
     card = await db.matchcard.update(where={"id": card_id}, data=query)
+    
+    # Проверка на наличие изменяемой игры
     if card is None:
         raise HTTPException(status_code=404, detail="No record could be found")
+    
     return card
